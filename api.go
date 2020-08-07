bf8ab358b002cc28a8354028f2bc8cf78e48d44e
daiana barbosa <daiana.soares@b2wdigital.com>
2020-08-06 23:42:47 -0300

    Atualizando a criação do endpoint /

--- api.go
+++ api.go
@@ -5,35 +5,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Status struct {
 	St string `json:"status"`
 }
 
+var status Status = Status{"ok"}
 var sttus Status
 
 func rotaPrincipal(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	sttus.St = "ok"
 	encoder := json.NewEncoder(w)
-	encoder.Encode(sttus)
-
-}
-
-func configurarRotas() {
-	http.HandleFunc("/", rotaPrincipal)
-
-}
-
-func configurarServidor() {
-	configurarRotas()
-
-	fmt.Println("Servidor esta rodando na porta 1337")
-	log.Fatal(http.ListenAndServe(":1337", nil))
+	encoder.Encode(status)
 
 }
 
 func main() {
-	configurarServidor()
+	fmt.Println("Servidor esta rodando na porta 8080")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/status", rotaPrincipal).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
