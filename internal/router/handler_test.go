c534ea0b9a6da214f72c57723a61567f6216e79a
daiana barbosa <daiana.soares@b2wdigital.com>
2020-08-21 17:30:19 -0300

    teste save handler

--- internal/router/handler_test.go
+++ internal/router/handler_test.go
@@ -1,8 +1,8 @@
 package router
 
 import (
-	"context"
 	"net/http"
+	"net/http/httptest"
 	"projeto-star-wars-api-go/internal/service/mocks"
 	"testing"
 
@@ -10,32 +10,37 @@ import (
 )
 
 func TestPlanetHandler_SavePlanet(t *testing.T) {
+	var rr *http.Request
+
+	//planetJSON, _ := json.Marshal(planetToCreate)
+	//r = planetJSON
+
 	type fields struct {
 		service *mocks.Planet
 	}
 	type args struct {
-		w http.ResponseWriter
 		r *http.Request
 	}
+
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		wantErr       bool
-		mock          func(fs *mocks.Planet)
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		mock    func(fs *mocks.Planet)
 	}{
 		{name: "sucesss",
 			fields: fields{
 				service: new(mocks.Planet),
 			},
-			args: args{r:,
-				w: },
-				wantErr: false,
-				mock: func(fs *mocks.Planet) {
-				fs.On("SavePlanet", mock.Anything, mock.Anything).
-					Return(nil)
+			args: args{
+				r: &http.Request{},
 			},
-
+			wantErr: false,
+			mock: func(fs *mocks.Planet) {
+				fs.On("SavePlanet", mock.Anything, mock.Anything).
+					Return(http.StatusCreated)
+			},
 		},
 	}
 	for _, tt := range tests {
@@ -44,8 +49,8 @@ func TestPlanetHandler_SavePlanet(t *testing.T) {
 			p := &PlanetHandler{
 				service: tt.fields.service,
 			}
-			//err := p.SavePlanet(tt.args.w, tt.args.r)
-
+			w := httptest.NewRecorder()
+			p.SavePlanet(w, tt.args.r)
 
 			tt.fields.service.AssertExpectations(t)
 
