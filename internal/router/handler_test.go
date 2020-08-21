196855f2b9763bc774aa345b1098ae59ebe463c6
daiana barbosa <daiana.soares@b2wdigital.com>
2020-08-21 16:55:11 -0300

    teste save handler

--- internal/router/handler_test.go
+++ internal/router/handler_test.go
@@ -1,34 +1,53 @@
 package router
 
 import (
-	"encoding/json"
+	"context"
 	"net/http"
-	"projeto-star-wars-api-go/internal/service"
+	"projeto-star-wars-api-go/internal/service/mocks"
 	"testing"
+
+	"github.com/stretchr/testify/mock"
 )
 
 func TestPlanetHandler_SavePlanet(t *testing.T) {
 	type fields struct {
-		service service.Planet
+		service *mocks.Planet
 	}
 	type args struct {
 		w http.ResponseWriter
 		r *http.Request
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    json.Decoder
-		wantErr bool
+		name   string
+		fields fields
+		args   args
+		wantErr       bool
+		mock          func(fs *mocks.Planet)
 	}{
-		// TODO: Add test cases.
+		{name: "sucesss",
+			fields: fields{
+				service: new(mocks.Planet),
+			},
+			args: args{r:,
+				w: },
+				wantErr: false,
+				mock: func(fs *mocks.Planet) {
+				fs.On("SavePlanet", mock.Anything, mock.Anything).
+					Return(nil)
+			},
+
+		},
 	}
 	for _, tt := range tests {
+		tt.mock(tt.fields.service)
 		t.Run(tt.name, func(t *testing.T) {
-			s := &PlanetHandler{
+			p := &PlanetHandler{
 				service: tt.fields.service,
 			}
+			//err := p.SavePlanet(tt.args.w, tt.args.r)
+
+
+			tt.fields.service.AssertExpectations(t)
 
 		})
 	}
