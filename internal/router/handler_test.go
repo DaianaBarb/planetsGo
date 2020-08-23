377fac4585d1d3911e5d02c41e61f7a2db499892
daiana barbosa <daiana.soares@b2wdigital.com>
2020-08-23 00:24:11 -0300

    teste deleteById handler

--- internal/router/handler_test.go
+++ internal/router/handler_test.go
@@ -1,13 +1,16 @@
 package router
 
 import (
-	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"projeto-star-wars-api-go/internal/service/mocks"
 	"strings"
 	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/stretchr/testify/mock"
 )
@@ -71,6 +74,52 @@ func TestPlanetHandler_SavePlanet(t *testing.T) {
 
 			tt.fields.service.AssertExpectations(t)
 
+		})
+	}
+}
+
+func TestPlanetHandler_DeleteById(t *testing.T) {
+	id := primitive.NewObjectID()
+	type fields struct {
+		service *mocks.Planet
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name               string
+		fields             fields
+		args               args
+		wantHttpStatusCode int
+		mock               func(fs *mocks.Planet)
+	}{
+		{
+			name: "sucesss",
+			fields: fields{
+				service: new(mocks.Planet),
+			},
+			args: args{
+
+				id: id.Hex(),
+			},
+			wantHttpStatusCode: http.StatusOK,
+			mock: func(fs *mocks.Planet) {
+				fs.On("DeleteById", mock.Anything, mock.Anything).Return(mock.Anything, nil).Once()
+			}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &PlanetHandler{
+				service: tt.fields.service,
+			}
+			request := httptest.NewRequest(http.MethodDelete, "/planets/"+id.Hex(), nil)
+			recorder := httptest.NewRecorder()
+
+			p.DeleteById(recorder, request)
+
+			assert.Equal(t, tt.wantHttpStatusCode, recorder.Code)
+
+			tt.fields.service.AssertExpectations(t)
 		})
 	}
 }
